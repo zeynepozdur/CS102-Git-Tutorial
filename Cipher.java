a25202d2e09951137740137e2c02f0be4cfcd935
zeynepp <zeynep.ozdur@ug.bilkent.edu.tr>
2025-02-05 23:04:50 +0300

    code is completed now

--- Cipher.java
+++ Cipher.java
@@ -15,9 +15,9 @@ public class Cipher
         // for all chars in the input string
         for (int i = 0; i < inputString.length(); i++)   
         {
-
+            outputString += replaceChar(inputString.charAt(i), true); 
         }
-
+        
         return outputString;
     }
 
@@ -25,9 +25,10 @@ public class Cipher
         
         // output string will be collected in this variable, one char at a time
         String outputString = "";
-        
-        replaceChar('a',true);
-        
+        for (int i = 0; i < inputString.length(); i++) 
+        {
+            outputString += replaceChar(inputString.charAt(i), false);
+        }
         return outputString;
     }
 
@@ -42,7 +43,7 @@ public class Cipher
             for (int i = 0; i < ORIGINAL_ALPHABET.length(); i++)   
             {
                 if(ORIGINAL_ALPHABET.charAt(i) == inputChar) {
-
+                    return CIPHER_ALPHABET.charAt(i);
                 }
             }
         }
